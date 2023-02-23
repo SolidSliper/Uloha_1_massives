b6d920efc5d91c4aff5a3137463c5efc5a36fe23
SolidSliper <85990847+SolidSliper@users.noreply.github.com>
2023-02-23 21:44:08 +0100

    Uloha1_massives_intersec_union_KRETSUL

---
+++ Source.cpp
@@ -0,0 +1,108 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(0));
+    int* set1;
+    int* set2;
+    int* intersec;
+    int* unio;
+    int n;
+
+    printf("Enter the size of 2 arrays -> ");
+    scanf("%i", &n);
+
+    //Deklarácia dynamických polí, aby ste si sami mohli nastaviť veľkosť množín
+    set1 = (int*)malloc(n * sizeof(int));
+    set2 = (int*)malloc(n * sizeof(int));
+    intersec = (int*)malloc(n * sizeof(int));
+    unio = (int*)malloc(2 * n * sizeof(int));
+
+    int i, j, k1 = 0, k2 = 0;
+
+    for (i = 0; i < n; i++) {
+        set1[i] = rand() % 10;
+        set2[i] = rand() % 10;
+    }
+
+    printf("First array: ");
+    for (i = 0; i < n; i++) {
+        printf("%i ", set1[i]);
+    }
+    printf("\n");
+
+    printf("Second array: ");
+    for (i = 0; i < n; i++) {
+        printf("%i ", set2[i]);
+    }
+    printf("\n");
+
+    //Cyklus pre najdenia intersection, + podmienky, čísla boli jedinečné
+    for (i = 0; i < n; i++) { 
+        for (j = 0; j < n; j++) {
+            if (set1[i] == set2[j]) {
+                int t = 0;
+                for (int k = 0; k < k1; k++) {
+                    if (intersec[k] == set1[i]) {
+                        t = 1;
+                        break;
+                    }
+                }
+                if (t==0) {
+                    intersec[k1] = set1[i];
+                    k1++;
+                }
+            }
+        }
+    }
+    //Cyklus pre najdenia union zo prvou množinou, + podmienky, abi čísla boli jedinečné
+    for (i = 0; i < n; i++) { 
+        int t = 0;
+        for (j = 0; j < n; j++) {
+            if (set2[i] == unio[j]) { t = 1;  break; }
+        }
+        if (t == 0) {
+            unio[k2] = set2[i];
+            k2++;
+        }
+        if (k2 > 1 && unio[k2 - 1] == unio[k2 - 2]) {
+            k2--;
+        }
+    }
+
+    //Cyklus pre najdenia union zo druhou množinou, + podmienky, abi čísla boli jedinečné
+    for (i = 0; i < n; i++) {
+        int t = 0;
+        for (j = 0; j < n; j++) {
+            if (set1[i] == unio[j]) { t = 1;  break; }
+        }
+        if (t == 0) {
+            unio[k2] = set1[i];
+            k2++;
+        }
+        if (k2 > 1 && unio[k2 - 1] == unio[k2 - 2]) {
+            k2--;
+        }
+    }
+
+    printf("Intersection: "); //výstup intersection
+    for (i = 0; i < k1; i++) {
+        printf("%d ", intersec[i]);
+    }
+    printf("\n");
+
+    printf("Union: ");  //výstup union
+    for (i = 0; i < k2; i++) {
+        printf("%d ", unio[i]);
+    }
+    printf("\n");
+
+    free(set1);
+    free(set2);
+    free(intersec);
+    free(unio);
+
+    return 0;
+}
+
